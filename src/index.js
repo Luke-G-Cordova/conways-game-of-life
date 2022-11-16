d99650f5424f8a42bd62ccb0edf76f7f20adeb72
Luke Cordova <lukethegreat000@gmail.com>
2022-11-16 03:49:13 -0800

    save

--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@ const canvas = document.querySelector('#myCanvas');
 canvas.width = 900;
 canvas.height = 900;
 const ctx = canvas.getContext('2d');
+ctx.strokeStyle = 'grey';
 let grid = [];
 const gridOrigin = [];
-const cellSize = 3;
+let cellSize = 3;
+let pause = true;
 for (let y = 0; y <= canvas.width / cellSize; y++) {
   for (let x = 0; x <= canvas.height / cellSize; x++) {
     if (grid[y] != null) {
@@ -21,8 +23,11 @@ let gridCopy = JSON.parse(JSON.stringify(gridOrigin));
 const loop = () => {
   ctx.fillStyle = 'white';
   ctx.fillRect(0, 0, canvas.width, canvas.height);
+  ctx.fillStyle = 'black';
   drawGrid();
-  calculateGrid();
+  if (!pause) {
+    calculateGrid();
+  }
 };
 
 const calculateGrid = () => {
@@ -77,33 +82,25 @@ const drawGrid = () => {
   grid.forEach((col, y) => {
     col.forEach((val, x) => {
       if (val === 1) {
-        ctx.fillStyle = 'black';
         ctx.fillRect(x * cellSize, y * cellSize, cellSize, cellSize);
       }
+      ctx.strokeRect(x * cellSize, y * cellSize, cellSize, cellSize);
     });
   });
 };
 
-// drawGrid();
-// calculateGrid();
-// drawGrid();
-// calculateGrid();
-// drawGrid();
-// console.log(getLiveNeighborCount(31, 30));
-
-grid[30][30] = 1;
-grid[31][31] = 1;
-grid[31][32] = 1;
-grid[30][32] = 1;
-grid[29][32] = 1;
-
 let interval = setInterval(loop, 50);
+window.addEventListener('keypress', (e) => {
+  if (e.key === 'a') {
+    pause = !pause;
+  }
+});
 
 window.addEventListener('mousedown', (e) => {
   let box = canvas.getBoundingClientRect();
   let mouseX = Math.floor((e.clientX - box.x) / cellSize);
   let mouseY = Math.floor((e.clientY - box.y) / cellSize);
-  grid[mouseY][mouseX] = 1;
+  grid[mouseY][mouseX] = grid[mouseY][mouseX] === 1 ? 0 : 1;
   window.onmousemove = (ev) => {
     let box = canvas.getBoundingClientRect();
     let mouseX = Math.floor((ev.clientX - box.x) / cellSize);
